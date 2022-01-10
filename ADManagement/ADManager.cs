ba7bafd7e5e2e0a52e2ce3ab9fe4836717370a88
Dragon <Dragon@I72600K-PC>
2022-01-10 19:58:21 +0100

    Projektdateien hinzufügen.

---
+++ ADManagement/ADManager.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * 
+ * Quelle: https://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C
+ * 
+ * 
+ * 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADManagement
+{
+    [SupportedOSPlatform("windows")]
+    public static class ADManager
+    {
+        /// <summary>
+        /// Rename an object and specify the domain controller and credentials directly
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <param name="objectDn"></param>
+        /// <param name="newName"></param>
+        public static void Rename(string server,
+            string userName, string password, string objectDn, string newName)
+        {
+            DirectoryEntry child = new("LDAP://" + server + "/" +
+                objectDn, userName, password);
+            child.Rename("CN=" + newName);
+        }
+        
+        /// <summary>
+        /// Translate the Friendly Domain Name to Fully Qualified Domain
+        /// </summary>
+        /// <param name="friendlyDomainName"></param>
+        /// <returns></returns>
+        public static string FriendlyDomainToLdapDomain(string friendlyDomainName)
+        {
+            string? ldapPath = null;
+            try
+            {
+                DirectoryContext objContext = new DirectoryContext(
+                    DirectoryContextType.Domain, friendlyDomainName);
+                Domain objDomain = Domain.GetDomain(objContext);
+                ldapPath = objDomain.Name;
+            }
+            catch (DirectoryServicesCOMException e)
+            {
+                ldapPath = e.Message.ToString();
+            }
+            return ldapPath;
+        }
+
+
+    }
+}
---
+++ ADManagement/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADManagement.Model
+{
+    public class User
+    {
+        public string Cn { get; set; }
+        public string Mail { get; set; }
+        public Group MemberOf { get; set; } = new Group();
+
+
+    }
+}
